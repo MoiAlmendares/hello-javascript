2c885451aa1ad39ff5c1e852538fc6b829f340b8
MoiAlmendares <moises.almendares15@gmail.com>
2025-07-16 06:15:20 -0600

    Ejercicios y pruebas

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -2,16 +2,37 @@
 Clase 20 - Ejercicios: Operadores
 Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 */
-
+let a = 5
+let b = 10
 // 1. Crea una variable para cada operación aritmética
+let suma = a + b
+let resta = a - b
+let multi = a * b
+let div = a / b
+let res = a % b
+let exponente = a ** b
+let Incremento = 0
+let Decremento = 0
 
 // 2. Crea una variable para cada tipo de operación de asignación,
-//    que haga uso de las variables utilizadas para las operaciones aritméticas
+//  que haga uso de las variables utilizadas para las operaciones aritméticas
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+console.log(suma)
+console.log(resta)
+console.log(multi)
+console.log(div)
+console.log(res)
+console.log(exponente)
+console.log(Incremento++)
+console.log(Decremento--)
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
-
+console.log(suma == resta)
+console.log(resta > div)
+console.log(div == multi)
+console.log(multi == exponente)
+console.log(exponente == Incremento)
 // 5. Utiliza el operador lógico and
 
 // 6. Utiliza el operador lógico or
