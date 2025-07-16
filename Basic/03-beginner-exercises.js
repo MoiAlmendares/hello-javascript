4fe4f5ee9ab4bb53fc9824128c59f0b4b1fbe1b4
MoiAlmendares <moises.almendares15@gmail.com>
2025-07-16 05:50:37 -0600

    Ejercicio de string type

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,18 +4,46 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
-
+// Mondongo exercise
 // 2. Escribe un comentario en varias líneas
-
+/* Mondongo exercise 2*/
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
-
+let stringType = 'Variable string'
+let bigIntType = 123123121231231231231233n
+let booleanType = true
+let numberType = 13.2
+let nullType = null
+let symbolType = Symbol("Simbolo")
 // 4. Imprime por consola el valor de todas las variables
-
+console.log(stringType)
+console.log(bigIntType)
+console.log(booleanType)
+console.log(numberType)
+console.log(nullType)
+console.log(numberType)
+console.log(symbolType)
 // 5. Imprime por consola el tipo de todas las variables
-
+console.log(typeof(stringType))
+console.log(typeof(bigIntType))
+console.log(typeof(booleanType))
+console.log(typeof(numberType))
+console.log(typeof(nullType))
+console.log(typeof(numberType))
+console.log(typeof(symbolType))
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
-
+stringType = 'String modificado'
+bigIntType = 2318712983712937192837
+booleanType = false
+numberType = 534.123
+nullType = null
+symbolType = Symbol('Nuevo valor para symbol')
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+stringType = 12
+bigIntType = 'string ahora'
+booleanType = Symbol("Antes boolean")
+numberType  = true
+nullType = 12037123192837n
+symbolType = null
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
